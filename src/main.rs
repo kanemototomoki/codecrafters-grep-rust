dbcaf153b733b45422b78482eedb4ece78fd02c4
kanemototomoki <kanemototomoki@gmail.com>
2023-01-05 16:01:27 +0900

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,6 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
 }
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
@@ -24,10 +21,9 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
 }
